bfe143c144b11699933006a1ef0cb887b740ea42
Bryan <Bryanortiz156@hotmail.com>
2023-11-18 18:14:35 -0500

    fixed

--- index.js
+++ index.js
@@ -73,8 +73,6 @@ var server = http.createServer(function(req, res) {
     }
 });
 
-var port = 3000;
-server.listen(port, function() {
-    console.log('Servidor escuchando en http://localhost:' + port);
-});
+/* var port = 3000; */
+server.listen();
 
